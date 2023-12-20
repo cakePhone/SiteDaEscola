e313125e50c476ff6d7efe3c16098a0c2dd337b3
cakePhone <ms8042939@gmail.com>
2023-12-20 21:04:15 +0000

    Volta com a configuração originaç

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from "@sveltejs/adapter-static";
-import { vitePreprocess } from "@sveltejs/kit/vite";
-import { importAssets } from "svelte-preprocess-import-assets";
-
-const dev = process.argv.includes("dev");
+import {vitePreprocess} from "@sveltejs/kit/vite";
+import {importAssets} from "svelte-preprocess-import-assets";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,25 +8,11 @@ const config = {
   // for more information about preprocessors
   preprocess: [vitePreprocess({}), importAssets()],
 
-  vitePlugin: {
-    inspector: true,
-  },
-
   kit: {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter({
-      pages: "build",
-      assets: "build",
-      fallback: undefined,
-      precompress: false,
-      strict: true,
-    }),
-
-    paths: {
-      base: dev ? "" : process.env.BASE_PATH,
-    },
+    adapter: adapter(),
   },
 };
 
