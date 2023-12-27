da8561a2831ec9aa02a7eb38e3294a91bc257286
cakePhone <ms8042939@gmail.com>
2023-12-27 12:01:30 +0000

    Adiciona titulo à página IDEIA

--- src/routes/galaxias/ideia-pt/+page.svelte
+++ src/routes/galaxias/ideia-pt/+page.svelte
@@ -2,6 +2,10 @@
   import {handleAnchorClick} from "$lib";
 </script>
 
+<svelte:head>
+  <title>IDEIA.pt</title>
+</svelte:head>
+
 <article>
   <section class="prose">
     <h1>IDEIA.pt</h1>
@@ -9,16 +13,19 @@
     <p>
       O gabinete IDEIA.pt insere-se no Departamento de Apoio e Orientação Educativa (DAOE) e tem como filosofia “Uma
       Escola para Todos e com Todos”.
-      <br>
+    </p>
+    <p>
       É um projeto que tem como objetivo a promoção do sucesso educativo e a plena integração escolar dos alunos e a sua
       orientação vocacional, tendo em conta os direitos e deveres dos alunos, conjugando a sua atividade com as demais
       estruturas de orientação educativa. Pretende contribuir para o desenvolvimento harmonioso e global dos
       adolescentes e jovens através de uma articulação entre a escola, a família e a comunidade, no que diz respeito a
       situações de risco como o abandono, o absentismo, o insucesso escolar e a adoção de comportamentos de risco.
-      <br>
+    </p>
+    <p>
       Tem como missão capacitar a comunidade escolar numa gestão de conflitos mais assertiva e eficaz, promovendo uma
       melhor comunicação entre todos e relações interpessoais mais satisfatórias.
-      <br>
+    </p>
+    <p>
       O gabinete IDEIA.pt é constituído por uma equipa de docentes que colabora com as demais estruturas da escola,
       entidades do concelho e é orientado pelo professor do grupo 300, João A. D. Lopes.
     </p>
@@ -37,11 +44,11 @@
     <p>
       O gabinete IDEIA.pt desenvolve a sua atividade de acordo com um plano anual de atividades que se integra no plano
       anual de atividades da escola.
-      <br>
+    </p>
+    <p>
       O gabinete encontra-se aberto diariamente e localiza-se no bloco central; fora do horário de funcionamento do
       gabinete IDEIA.pt, as ocorrências devem ser encaminhadas para os Serviços de Psicologia e Orientação (SPO) ou para
-      a
-      Direção da Escola.
+      a Direção da Escola.
     </p>
 
     <h2 id="principios">Princípios Orientadores</h2>
@@ -79,7 +86,7 @@
 
 <style lang="postcss">
   hr {
-    @apply max-w-[80ch]
+    @apply max-w-[80ch] opacity-20
   }
 
   .inline-indice {
