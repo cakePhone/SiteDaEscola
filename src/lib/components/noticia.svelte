ee4d301bdc0b2052a377449b47d7ef3ce8a31c42
cakePhone <ms8042939@gmail.com>
2023-12-31 18:21:11 +0000

    Adiciona prop para decidir se abre numa nova tab ou não

--- src/lib/components/noticia.svelte
+++ src/lib/components/noticia.svelte
@@ -6,27 +6,51 @@
   export let description: string;
   export let date: string = "Sem data";
   export let link: string;
+
+  export let openInNew: boolean = true;
 </script>
 
-<a href="{link}" class="card" target="#">
-  {#if image}
-    <img
-        src="{image}"
-        alt=""
-        class="w-full aspect-[1.5] object-cover rounded-md flex-shrink-0"
-    />
-  {:else}
-    <Icon icon="ic:round-error" class="h-52 w-full flex-shrink-0"/>
-  {/if}
+{#if openInNew}
+  <a href="{link}" class="card" target="_blank">
+    {#if image}
+      <img
+          src="{image}"
+          alt=""
+          class="w-full aspect-[1.5] object-cover rounded-md flex-shrink-0"
+      />
+    {:else}
+      <Icon icon="ic:round-error" class="h-52 w-full flex-shrink-0"/>
+    {/if}
 
-  <h2 class:h-full="{!description}">{title}</h2>
-  {#if description}
-    <p class="h-full max-h-32 line-clamp-2">
-      {description}
-    </p>
-  {/if}
-  <p class="text-right">Postado em: {date}</p>
-</a>
+    <h2 class:h-full="{!description}">{title}</h2>
+    {#if description}
+      <p class="h-full max-h-32 line-clamp-2">
+        {description}
+      </p>
+    {/if}
+    <p class="text-right">Postado em: {date}</p>
+  </a>
+{:else}
+  <a href="{link}" class="card">
+    {#if image}
+      <img
+          src="{image}"
+          alt=""
+          class="w-full aspect-[1.5] object-cover rounded-md flex-shrink-0"
+      />
+    {:else}
+      <Icon icon="ic:round-error" class="h-52 w-full flex-shrink-0"/>
+    {/if}
+
+    <h2 class:h-full="{!description}">{title}</h2>
+    {#if description}
+      <p class="h-full max-h-32 line-clamp-2">
+        {description}
+      </p>
+    {/if}
+    <p class="text-right">Postado em: {date}</p>
+  </a>
+{/if}
 
 <style lang="postcss">
   a {
